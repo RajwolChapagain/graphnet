1878b1cb4636603adccbeec666fa398c8613f5c7
Rajwol Chapagain <rchapagain@icecube.wisc.edu>
2025-11-30 05:04:56 +0000

    Add RadialGraph

--- src/graphnet/models/graphs/graphs.py
+++ src/graphnet/models/graphs/graphs.py
@@ -6,7 +6,7 @@ from numpy.random import Generator
 
 from .graph_definition import GraphDefinition
 from graphnet.models.detector import Detector
-from graphnet.models.graphs.edges import KNNEdges
+from graphnet.models.graphs.edges import KNNEdges, RadialEdges
 from graphnet.models.graphs.nodes import NodeDefinition, NodesAsPulses
 
 
@@ -57,6 +57,52 @@ class KNNGraph(GraphDefinition):
             **kwargs,
         )
 
+class RadialGraph(GraphDefinition):
+    """A Graph representation where Edges are drawn according to a radius."""
+
+    def __init__(
+        self,
+        detector: Detector,
+        node_definition: NodeDefinition = None,
+        input_feature_names: Optional[List[str]] = None,
+        dtype: Optional[torch.dtype] = torch.float,
+        perturbation_dict: Optional[Dict[str, float]] = None,
+        seed: Optional[Union[int, Generator]] = None,
+        radius: float = 200.0,
+        columns: List[int] = [0, 1, 2],
+        **kwargs: Any,
+    ) -> None:
+        """Construct radial graph representation.
+
+        Args:
+            detector: Detector that represents your data.
+            node_definition: Definition of nodes in the graph.
+            input_feature_names: Name of input feature columns.
+            dtype: data type for node features.
+            perturbation_dict: Dictionary mapping a feature name to a standard
+                               deviation according to which the values for this
+                               feature should be randomly perturbed. Defaults
+                               to None.
+            seed: seed or Generator used to randomly sample perturbations.
+                  Defaults to None.
+            radius: radius fed to edge definition
+            columns: node feature columns used for distance calculation
+            . Defaults to [0, 1, 2].
+        """
+        # Base class constructor
+        super().__init__(
+            detector=detector,
+            node_definition=node_definition or NodesAsPulses(),
+            edge_definition=RadialEdges(
+                radius=radius,
+                columns=columns,
+            ),
+            dtype=dtype,
+            input_feature_names=input_feature_names,
+            perturbation_dict=perturbation_dict,
+            seed=seed,
+            **kwargs,
+        )
 
 class EdgelessGraph(GraphDefinition):
     """A Data representation without edge assignment.
